4902361e908545e33bfd52494a1cf462b112a7bc
mosespace <kiskayemoses@gmail.com>
2025-01-27 21:11:07 +0000

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './dist/styles.css'; // Import the compiled CSS
+
 export { Toaster } from './components/toaster';
 export { toast } from './toast';
 export type { ToasterProps } from './components/toaster';
