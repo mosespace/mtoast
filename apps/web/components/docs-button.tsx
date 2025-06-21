9f8e44f169a188ad0963d76be51901fba8c9c25f
mosespace <kiskayemoses@gmail.com>
2025-06-21 16:29:05 +0100

    feat: initial commit

--- apps/web/components/docs-button.tsx
+++ apps/web/components/docs-button.tsx
@@ -12,7 +12,7 @@ const DocsButton = () => {
     <Link
       href={'/docs'}
       prefetch={true}
-      className="fixed bottom-[30px] right-[30px] z-30 flex justify-center items-center w-[60px] h-[60px] bg-amber-500 rounded-full"
+      className="fixed bottom-[30px] left-[30px] z-30 flex justify-center items-center w-[60px] h-[60px] bg-amber-500 rounded-full"
     >
       <FileText size={28} color="white" />
       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-amber-500/70 opacity-75"></span>
