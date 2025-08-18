4da7b038b10b85c4f1f4161ec7fd93d8f5fb2b85
mosespace <kiskayemoses@gmail.com>
2025-08-18 22:26:50 +0100

    fix: fixed error in production relating to framer motion

--- apps/web/.source/index.ts
+++ apps/web/.source/index.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_2 from "../content/docs/toaster.mdx?collection=docs&hash=1745776312190"
-import * as docs_1 from "../content/docs/toast.mdx?collection=docs&hash=1745776312190"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1745776312190"
+import * as docs_2 from "../content/docs/toaster.mdx?collection=docs&hash=1755550450898"
+import * as docs_1 from "../content/docs/toast.mdx?collection=docs&hash=1755550450898"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1755550450898"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"D:/packages/mtoast/apps/web/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"toast.mdx","absolutePath":"D:/packages/mtoast/apps/web/content/docs/toast.mdx"}, data: docs_1 }, { info: {"path":"toaster.mdx","absolutePath":"D:/packages/mtoast/apps/web/content/docs/toaster.mdx"}, data: docs_2 }], [{"info":{"path":"meta.json","absolutePath":"D:/packages/mtoast/apps/web/content/docs/meta.json"},"data":{"title":"Basics","pages":["---Introduction---","index","---API---","toast","toaster"],"description":"The docs for the toast","root":true,"icon":"Building2"}}])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/mosespace/Desktop/projects/mtoast/apps/web/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"toast.mdx","absolutePath":"/home/mosespace/Desktop/projects/mtoast/apps/web/content/docs/toast.mdx"}, data: docs_1 }, { info: {"path":"toaster.mdx","absolutePath":"/home/mosespace/Desktop/projects/mtoast/apps/web/content/docs/toaster.mdx"}, data: docs_2 }], [{"info":{"path":"meta.json","absolutePath":"/home/mosespace/Desktop/projects/mtoast/apps/web/content/docs/meta.json"},"data":{"title":"Basics","pages":["---Introduction---","index","---API---","toast","toaster"],"description":"The docs for the toast","root":true,"icon":"Building2"}}])
